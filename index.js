a70f43be0813003d21f7dcaf23334c315a3f109c
Christoph Geypen <geypenchristoph@gmail.com>
2020-04-23 12:23:34 +0200

    get authentication working using .env file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').load();
 const escapeHtml = require('escape-html');
 const API = require('call-of-duty-api')();
 
-const cod = API.login(process.env.email, process.env.password);
+API.login(process.env.EMAIL, process.env.PASSWORD).then((output) => console.log(output)).catch((err) => console.log(err));
+
 
 /**
  * HTTP Cloud Function.
@@ -12,14 +12,22 @@ const cod = API.login(process.env.email, process.env.password);
  * @param {Object} res Cloud Function response context.
  *                     More info: https://expressjs.com/en/api.html#res
  */
+
+// node -r dotenv/config index.js
 // gcloud functions deploy codApi --runtime nodejs8 --trigger-http  --region europe-west1
+
 exports.codApi = (req, res) => {
-  const stats = cod.MWstats('christoph0088', API.platforms.psn).then((output) => {
+  const userName = req.query.name || 'christoph0088';
+
+  console.log(userName);
+  console.log(process.env.EMAIL);
+  console.log(process.env.PASSWORD);
+
+  const stats = API.MWstats(userName, API.platforms.psn).then((output) => {
     console.log(output);
-  })
-    .catch((err) => {
-      console.log(err);
-    });
+    res.send(stats);
+  }).catch((err) => {
+    console.log(err);
+  });
   // res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
-  res.send(stats);
 };
