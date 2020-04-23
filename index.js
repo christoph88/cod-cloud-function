b2d1a7dbe1322bbbdedc2235a1f17c6f054802b3
Christoph Geypen <geypenchristoph@gmail.com>
2020-04-23 11:30:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// gcloud functions deploy codApi --runtime nodejs8 --trigger-http  --region europe-west1
+
+const escapeHtml = require('escape-html');
+
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloHttp = (req, res) => {
+  res.send(`Hello ${escapeHtml(req.query.name || req.body.name || 'World')}!`);
+};
